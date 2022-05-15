f803ee475ae9c16038ae82945852c59149d1fe11
Мишкин Олексiй <lexamishkin72@gmail.com>
2022-05-16 01:13:09 +0400

    Added functions for adding vertixies and connecting them between each other

--- headers/Graph.h
+++ headers/Graph.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template <class TVertex, class TEdge>
 class Graph
 {
@@ -6,6 +8,7 @@ private:
 	{
 		TVertex destination;
 		TEdge edge_entity;
+		Edge* next;
 	};
 	struct Vertex
 	{
@@ -17,7 +20,7 @@ private:
 	size_t _size;
 	size_t _capacity;
 	//
-	int contains(const TVertex& vertex) const //returns and index of the element plus 1
+	size_t contains(const TVertex& vertex) const //returns and index of the element plus 1
 	{
 		if (_table)
 			for (size_t i = 0; i < _size; i++)
@@ -25,7 +28,7 @@ private:
 					return i + 1;
 		return 0;
 	}
-	size_t capacity_resize(const size_t& delta_capacity)
+	size_t capacity_resize(const short& delta_capacity)
 	{
 		if (_capacity + delta_capacity <= _size) throw "Too small capacity for current amount of elemets.";
 
@@ -41,7 +44,7 @@ private:
 	}
 
 public:
-	Graph() : _table(nullptr), _size(0), _capacity(8) {}
+	Graph() : _table(nullptr), _size(0), _capacity(0) {}
 	Graph(const Graph& copygraph)
 	{
 		this->_size = copygraph._size;
@@ -57,32 +60,92 @@ public:
 			delete[] _table;
 	}
 
-	void add_vertex(const Vertex& vertex)
+	void add_vertex(const TVertex& vertex)
 	{
-		if (_size < _capacity - 1)
+		if (_size < _capacity - 1 && _table)
 		{
 			if (!contains(vertex))
 			{
+				Vertex adding = { vertex, nullptr };
+				_table[_size] = adding;
 				_size++;
-				_table[_size] = vertex;
 			}
 		}
 		else
 		{
 			capacity_resize(8);
+			Vertex adding = { vertex, nullptr };
+			_table[_size] = adding;
 			_size++;
-			_table[_size] = vertex;
 		}
 	}
-	void delete_vertex(const Vertex& vertex)
+	bool delete_vertex(const TVertex& vertex)
 	{
-		for (int i = contains(vertex) - 1; i < _size - 1; i++)
+		size_t index = contains(vertex);
+		if (index)
 		{
-			_table[i] = _table[i + 1];
-			_size--;
-		}
+			for (size_t i = index - 1; i < _size - 1; i++)
+			{
+				_table[i] = _table[i + 1];
+			}
 
-		if (_capacity - _size >= 8)
-			capacity_resize(-8);
+			_size--;
+			if (_capacity - _size >= 8)
+				capacity_resize(-8);
+			return true;
+		}
+		return false;
 	}
+	bool connect(const TVertex& from, const TVertex& to, const TEdge& weight)
+	{
+		size_t from_index = contains(from);
+		size_t to_index = contains(to);
+		if (from_index && to_index)
+		{
+			Edge* temp = _table[from_index - 1].first;
+			if (!temp)
+			{
+				//Initialize first edge of the vertex
+				_table[from_index - 1].first = new Edge;
+				_table[from_index - 1].first->destination = to;
+				_table[from_index - 1].first->edge_entity = weight;
+				_table[from_index - 1].first->next = nullptr;
+			}
+			else
+			{
+				//Append new edge of the vertex
+				while (temp->next)
+					temp = temp->next;
+				temp->next = new Edge;
+				temp->next->destination = to;
+				temp->next->edge_entity = weight;
+				temp->next->next = nullptr;
+			}
+			return true;
+		}
+		else
+			return false;
+	}
+
+	template <class v, class e>
+	friend std::ostream& operator<<(std::ostream& os, const Graph<v, e>& graph);
 };
+
+template <class TVertex, class TEdge>
+std::ostream& operator<<(std::ostream& os, const Graph<TVertex, TEdge>& graph)
+{
+	for (size_t i = 0; i < graph._size; i++)
+	{
+		os << graph._table[i].element << "\n";
+		Graph<TVertex, TEdge>::Edge* temp = graph._table[i].first;
+
+		while (temp)
+		{
+			os << "-" << temp->edge_entity << "-" << temp->destination << std::endl;
+			temp = temp->next;
+		}
+		os << std::endl;
+	}
+
+	return os;
+}
--- ui/ui.cpp
+++ ui/ui.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
 #include "../headers/Graph.h"
+
 
 int main()
 {
 	Graph<int, int> test_graph;
+	test_graph.add_vertex(1);
+	test_graph.add_vertex(2);
+	test_graph.add_vertex(3);
+	test_graph.add_vertex(4);
+	std::cout << test_graph << std::endl;
 	return 0;
 }
